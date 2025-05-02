6349bbd294065e63ebde7106b92c323de4788a71
YASSER-ABOOH <110708576+YASSER-ABOOH@users.noreply.github.com>
2025-05-02 10:34:15 +0530

    update

---
+++ assessments/week03/week03/problem03.cpp
@@ -0,0 +1,16 @@
+//print the count of smaller element right to the current element
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,i;
+	cout <<"Number of Elements"<< n;
+	cin >> n;
+	cout << "Enter Elements" << endl;
+	int a[10];
+	for (i = 0;i < n;i++)
+		cin >> a[i];
+}
