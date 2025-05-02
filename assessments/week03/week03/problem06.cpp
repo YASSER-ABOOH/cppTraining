e1cb731a5d463ccac441d5d4c7f2b0b4d1b55631
YASSER-ABOOH <110708576+YASSER-ABOOH@users.noreply.github.com>
2025-05-02 13:22:13 +0530

    4 completed....maybe minor corrections/errors

--- assessments/week03/week03/problem06.cpp
+++ assessments/week03/week03/problem06.cpp
@@ -6,23 +6,27 @@ int main() {
     int n,r,a[100];
     cout << "Number of elements"<<endl;
     cin >> n;
+    cout << "Enter elements" << endl;
     for (int i = 0; i < n; i++) 
     {
-        cout << "Enter elements"<<endl;
         cin >> a[i];
     }
     cout << "Enter for subarray" << endl;
     cin >> r;
-    for (int i = 0; i < n; i += i) {
-        int lt = i;
-        int rt = (i + i - 1 < n) ? (i + i - 1) : (n - 1);
-
+    //for (int i = 0; i < n; i += i)
+    for(int i=0;i<n;i+=r)//for groups of 3
+    {
+        int rt, lt = i;
+        rt = i + r - 1;
+        if (rt >= n) {
+            rt = n - 1;
+        }
         while (lt < rt) {
-            int temp = a[lt];
+            int c = a[lt];//assigning a temp variable to swap the values of left and right 
             a[lt] = a[rt];
-            a[rt] = temp;
-            lt++;
-            rt--;
+            a[rt] = c;
+            lt++;//move left
+            rt--;//move right
         }
     }
     for (int i = 0; i < n; i++) {
