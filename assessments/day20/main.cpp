54ecbba237370972c64b220dc986b3ba521e7dd0
YASSER-ABOOH <110708576+YASSER-ABOOH@users.noreply.github.com>
2025-05-13 11:20:36 +0530

    up

--- assessments/day20/main.cpp
+++ assessments/day20/main.cpp
@@ -1,18 +1,65 @@
 //student managenment system
 
 #include<iostream>
-
-void std_data(int std_id, char std_name[50], int std_ph_1, int std_ph_2, char std_add[300], char std_dept[10], char std_mail[50], int tenth_percent, int twelfth_percent, char quota[10], char category[10]);
+#include<string>
+#include<cstring>
 
 using namespace std;
 
 
-
 int main()
 {
-	cout << "STUDENT ADMISSION SYSTEM"<<endl;
-	cout << "========================";
-	//input datas
+
+	cout << "STUDENT ADMISSION SYSTEM" << endl;
+	cout << "========================" << endl;
+	//declaration
+	int std_id;
+	char std_name[50];
+	int std_ph_1;
+	int std_ph_2;
+	char std_add[300];
+	char std_dept[10];
+	char std_mail[50];
+	int tenth_percent;
+	int twelfth_percent;
+	char quota[10];
+	char category[10];
 	
+	//data input
+	cout << "Enter Student ID" << endl;
+	cin >> std_id;
+
+	cout << "Enter Student Name" << endl;
+    cin.getline(std_name, 50);
+
+	cout << "Enter Student's Primary Phone Number" << endl;
+	cin >> std_ph_1;
+
+	cout << "Enter Student's Secondary Phone Number"<<endl;
+	cin >> std_ph_2;
+
+	cout << "Enter Student Address"<<endl;
+	cin.getline(std_add, 300);
+
+	cout << "Enter Student Department"<<endl;
+	cin.getline(std_dept, 10);
+
+	cout << "Enter Student Mail ID" << endl;
+	cin.getline(std_mail, 50);
+
+	cout << "Enter 10th percentage" << endl;
+	cin >> tenth_percent;
+
+	cout << "Enter 12th percentage" << endl;
+	cin >> twelfth_percent;
+
+	cout << "Enter Quota" << endl;
+	cin.getline(quota, 10);
+
+	cout << "Enter Category" << endl;
+	cin.getline(category, 10);
+
+	cout << std_id << "\t" << std_name << "\t" << std_ph_1 << "\t" << std_ph_2 << "\t" << std_add << "\t" << std_dept << "\t" << std_mail << "\t" << tenth_percent << "\t" << twelfth_percent << "\t" << quota << "\t" << category;
+
 
 }
---
+++ assessments/day20/view.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int view(int)
+{
+	int n;
+	cin >> n;
+	switch (n)
+	{
+	case 1:
+	}
+}
