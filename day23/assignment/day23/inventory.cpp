0bf564729c7c3c591e92f2f875490a91749c013f
YASSER-ABOOH <110708576+YASSER-ABOOH@users.noreply.github.com>
2025-05-19 00:55:28 +0530

    car

--- day23/assignment/day23/inventory.cpp
+++ day23/assignment/day23/inventory.cpp
@@ -4,26 +4,26 @@ using namespace std;
 
 typedef struct carDetails
 {
-	int car_model_number;
-	string car_brand;
+	int car_model_number[100];
+	string car_brand[100];
 }DET;
 
 typedef struct carSpecification
 {
-	int car_model_number;
-	int car_quantity;
-	string car_category;
+	int car_model_number[100];
+	int car_quantity[100];
+	string car_category[100];
 }SPEC;
 
 typedef struct carPrice
 {
-	int car_model_number;
-	int off_cost;
+	int car_model_number[100];
+	int off_cost[100];
 	int resell_old_car;
-	int on_cost;
+	int on_cost[100];
 	int addon_part;
-	int addon_cost;
-	int old_car_cost;
+	int addon_cost[100];
+	int old_car_cost[100];
 }PRC;
 
 void display(DET&, SPEC&, PRC&);
@@ -48,17 +48,17 @@ int main()
 void getCarDetails(DET& d, SPEC& s, PRC& p)
 {
 	cout << "Enter car model number" << endl;
-	cin >> d.car_model_number;
+	cin >> d.car_model_number[100];
 	cout << "Enter car brand" << endl;
-	cin >> d.car_brand;
+	cin >> d.car_brand[100];
 	cout << "Enter car category" << endl;
-	cin >> s.car_category;
+	cin >> s.car_category[100];
 	cout << "Car Stock left" << endl;
-	cin >> s.car_quantity;
+	cin >> s.car_quantity[100];
 	cout << "Off road cost of car" << endl;
-	cin >> p.off_cost;
-	d.car_model_number = s.car_model_number;
-	s.car_model_number = p.car_model_number;
+	cin >> p.off_cost[100];
+	d.car_model_number[100] = s.car_model_number[100];
+	s.car_model_number[100] = p.car_model_number[100];
 	return;
 }
 
@@ -67,12 +67,12 @@ int carAdd(DET& d, PRC& p)
 	cout << "Enter Number of Parts to be added" << endl;
 	cin >> p.addon_part;
 	if (p.addon_part == 0)
-		p.addon_cost = 0;
+		p.addon_cost[100] = 0;
 	else {
 		cout << "Enter Add-on Cost" << endl;
-		cin >> p.addon_cost;
+		cin >> p.addon_cost[100];
 	}
-	return p.addon_cost;
+	return p.addon_cost[100];
 }
 
 
@@ -84,23 +84,23 @@ int carRe(DET&d,PRC&p)
 	do
 	{
 		if (p.resell_old_car == 0)
-			p.old_car_cost = 0;
+			p.old_car_cost[100] = 0;
 		else if (p.resell_old_car == 1)
 		{
 			cout << "Enter the value of old car" << endl;
-			cin >> p.old_car_cost;
+			cin >> p.old_car_cost[100];
 		}
 		else
 		{
 			cout << "Invalid Input.Enter '0' or '1' only" << endl;
 		}
 	} while (p.resell_old_car != 0 && p.resell_old_car != 1);
-	return p.old_car_cost;
+	return p.old_car_cost[100];
 }
 
 int carTotalCost(PRC& p)
 {
-	p.on_cost = p.off_cost + p.addon_cost-p.old_car_cost;
+	p.on_cost[100] = p.off_cost[100] + p.addon_cost[ - p.old_car_cost;
 	return p.on_cost;
 }
 void display(DET& d, SPEC& s, PRC& p)
